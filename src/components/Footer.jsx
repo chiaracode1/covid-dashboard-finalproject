345eb81d21755a682d2effd4eff3a2a0c0be7e0a
chiaracode <138113937+chiaracode1@users.noreply.github.com>
2024-05-18 18:30:54 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col justify-center items-center bg-gray-800 text-white py-4">
+    <footer className="flex flex-col justify-center items-center bg-gray-800 text-white fixed bottom-0 w-full">
       <div className="flex space-x-4">
         <a
           href="https://github.com/chiaracode1"
@@ -10,8 +10,7 @@ const Footer = () => {
           rel="noopener noreferrer"
           className="text-xl"
         >
-      
-          GitHub
+          <h2>Github</h2>
         </a>
         <a
           href="https://linkedin.com/in/chiaraceriola"
@@ -19,13 +18,13 @@ const Footer = () => {
           rel="noopener noreferrer"
           className="text-xl"
         >
-          LinkedIn
+          <h2>LinkedIn</h2>
         </a>
       </div>
       <div className="mt-2">
         <p className="text-sm">Powered by <a href="https://covid-api.com/" target="_blank" rel="noopener noreferrer" className="underline">COVID-19 API</a></p>
       </div>
-        <div className="mt-2">Chiara Ceriola ©</div>
+      <div className="mt-2">Chiara Ceriola ©</div>
     </footer>
   );
 };
