64e6e6ec567509fce43ce43e9b5afb7b1f7c23bc
chiaracode <138113937+chiaracode1@users.noreply.github.com>
2024-05-19 11:36:04 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,27 +2,31 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col justify-center items-center bg-gray-800 text-white fixed bottom-0 w-full mt-auto">
-      <div className="flex space-x-2">
+    <footer className="flex justify-between items-center bg-gray-800 text-white fixed bottom-0 w-full px-4 py-2">
+      <div className="flex space-x-4">
         <a
           href="https://github.com/chiaracode1"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-base sm:text-lg"
+          className="text-base"
         >
-         <h2 className="text-1xl">Github</h2>
+          <h2 className="">GitHub</h2>
         </a>
         <a
           href="https://linkedin.com/in/chiaraceriola"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-base sm:text-lg"
+          className="text-base"
         >
-          <h2 className="text-1xl">LinkedIn</h2>
+          <h2 className="">LinkedIn</h2>
         </a>
       </div>
-        <p className="text-xs sm:text-sm mt-auto">Powered by <a href="https://covid-api.com/" target="_blank" rel="noopener noreferrer" className="underline">COVID-19 API</a></p>
-      <div className="text-xs sm:text-sm mt-auto">Chiara Ceriola ©</div>
+      <div className="flex flex-col items-end">
+        <p className="text-xs sm:text-sm mr-0">
+          Powered by <a href="https://covid-api.com/" target="_blank" rel="noopener noreferrer" className="underline">COVID-19 API</a>
+        </p>
+        <p className="text-xs sm:text-sm">Chiara Ceriola ©</p>
+      </div>
     </footer>
   );
 };
